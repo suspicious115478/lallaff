9c92896e26facd6c08041c7ead083b7a815fe2aa
suspicious115478 <sus24picious45@gmail.com>
2025-12-02 11:57:34 +0530

    Update main.jsx

--- frontendd/src/main.jsx
+++ frontendd/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './index.css'; // <-- TailwindCSS import
 
 const el = document.getElementById('root');
 createRoot(el).render(<App />);
